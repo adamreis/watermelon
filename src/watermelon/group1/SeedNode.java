78566f443536296d616781039d0a3f6cba4b538a
Adam Reis <adamhreis@gmail.com>
2014-10-24 11:14:44 -0700

    fixed SeedNode implementation

--- src/watermelon/group1/SeedNode.java
+++ src/watermelon/group1/SeedNode.java
@@ -5,14 +5,16 @@ import java.util.ArrayList;
 import watermelon.group1.Location;
 
 public class SeedNode extends Location {
-	public ArrayList<SeedNode> adjacent;
 	public enum Ploidy {
 		NONE, DIPLOID, TETRAPLOID
 	};
 	
+	public ArrayList<SeedNode> adjacent;
+	public Ploidy ploidy;
+	
 	public SeedNode(Location l) {
 		super(l.x, l.y);
-		
 		adjacent = new ArrayList<SeedNode>();
+		ploidy = Ploidy.NONE;
 	}
 }
