b9d58d4629fe85fec36d47c1bb3062887f6f0c22
Adam Reis <adamhreis@gmail.com>
2014-10-24 11:16:14 -0700

    renamed Plody to Ploidies

--- src/watermelon/group1/SeedNode.java
+++ src/watermelon/group1/SeedNode.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 import watermelon.group1.Location;
 
 public class SeedNode extends Location {
-	public enum Ploidy {
+	public enum Ploidies {
 		NONE, DIPLOID, TETRAPLOID
 	};
 	
 	public ArrayList<SeedNode> adjacent;
-	public Ploidy ploidy;
+	public Ploidies ploidy;
 	
 	public SeedNode(Location l) {
 		super(l.x, l.y);
+		
 		adjacent = new ArrayList<SeedNode>();
-		ploidy = Ploidy.NONE;
+		ploidy = Ploidies.NONE;
 	}
 }
